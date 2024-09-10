fc874480d599aa32ae1b18d030c43795316d9c6c
nitindahiya-dev <nitindahiya00000@gmail.com>
2024-09-10 22:33:23 +0530

    under construction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Heading from "@/components/Heading";
+import UnderConstruction from "@/components/UnderConstruction";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col gap-5 items-center justify-center">
       <Heading />
+      <UnderConstruction />
     </div>
   );
 }
---
+++ src/components/UnderConstruction.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UnderConstruction = () => {
+  return (
+    <div>
+      <h1 className='text-5xl font-extrabold absolute bottom-60 text-red-500 border border-red-500 p-5 right-[37vw]'>Under Construction</h1>
+    </div>
+  )
+}
+
+export default UnderConstruction
