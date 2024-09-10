401f1f3b169d42a0cec595df50725a672c36dad2
nitindahiya-dev <nitindahiya00000@gmail.com>
2024-09-10 22:25:10 +0530

    setup and add heading

---
+++ src/components/Heading.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+
+const Heading = () => {
+  return (
+    <div className='flex flex-col items-center'>
+        <h1 className='text-5xl font-extrabold'>Solana Token Creator</h1>
+        <p className='text-lg'>Easily Create your own Solana SPL Token in just 7+1 steps without coding</p>
+    </div>
+  )
+}
+
+export default Heading
